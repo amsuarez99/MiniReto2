53721d9df1c54c796ed000b00ccc31441cb1eca0
Nestor <31547238+NestorRubio@users.noreply.github.com>
2021-05-09 22:45:40 -0600

    FUNCIONAN SIN ERRORES

---
+++ clienteJalador.c
@@ -0,0 +1,181 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+
+#define RESPONSE_SIZE 200
+#define MAX 50
+#define SERVER_PORT 8083
+
+typedef struct{
+    char username[MAX];
+    char password[MAX];
+} User;
+
+// return a user
+User inputUser() {
+    User temp;
+    printf("Introduce tus credenciales:\n");
+    printf("Usuario> ");
+    scanf("%s", temp.username);
+    printf("Contraseña> ");
+    scanf("%s", temp.password);
+
+
+    // Allocate memory for the pointers of the struct
+    // User *u = malloc(sizeof(struct User));
+
+    // // Allocate memory for contents of pointers.
+    // p->username = malloc(strlen(username) + 1);
+    // p->password = malloc(strlen(password) + 1);
+
+    // strcpy(u->name, username);
+    // strcpy(u->password, password);
+
+    return temp;
+}
+
+//Create a Socket for server communication
+short SocketCreate(void)
+{
+    short hSocket;
+    printf("Create the socket\n");
+    // AF_INET (protocol IPV4), SOCKET_STREAM (TCP), 0 = ip protocol
+    hSocket = socket(AF_INET, SOCK_STREAM, 0);
+    return hSocket;
+}
+
+// // Serializes the usr to a single buffer
+// int serialize_usr(unsigned char *buffer, struct User* usr) {
+//     int x;
+//     for(x = 0; x < strlen(usr->username); x++) {
+//         buffer[i] = *(usr->username + i);
+//     }
+//     for(int i = x; i < strlen(usr->password); i++) {
+//         buffer[i] = *(usr->password + i);
+//     }
+//     return buffer + 1;
+// }
+
+// Send the data to the server and set the timeout of 20 seconds
+int SocketSend(int hSocket,char* Rqst,short lenRqst)
+{
+    int shortRetval = -1;
+    struct timeval tv;
+    tv.tv_sec = 20;  /* 20 Secs Timeout */
+    tv.tv_usec = 0;
+    if(setsockopt(hSocket,SOL_SOCKET,SO_SNDTIMEO,(char *)&tv,sizeof(tv)) < 0)
+    {
+        printf("Time Out\n");
+        return -1;
+    }
+    shortRetval = send(hSocket, Rqst, lenRqst, 0);
+    return shortRetval;
+}
+
+//receive the data from the server
+int SocketReceive(int hSocket,char* Rsp,short RvcSize)
+{
+    int shortRetval = -1;
+    struct timeval tv;
+    tv.tv_sec = 20;  /* 20 Secs Timeout */
+    tv.tv_usec = 0;
+    if(setsockopt(hSocket, SOL_SOCKET, SO_RCVTIMEO,(char *)&tv,sizeof(tv)) < 0)
+    {
+        perror("Time out...\n");
+        exit(EXIT_FAILURE);
+    }
+    shortRetval = recv(hSocket, Rsp, RvcSize, 0);
+    return shortRetval;
+}
+
+int authenticate(int socket, const struct sockaddr *dest, socklen_t dlen, const User usr) {
+
+    unsigned char buffer[sizeof(User)], response[RESPONSE_SIZE] = {0};
+    memcpy(buffer, &usr, sizeof(buffer));
+    // , *ptr, response[RESPONSE_SIZE] = {0};
+    // ptr = serialize_usr(buffer, usr);
+
+    // 1: call succeeded
+    // 0: call failed
+    // if((sendto(socket, buffer, ptr - buffer, 0, dest, dlen) == ptr - bufer) == 0) {
+    //     perror("No se pudo conectar con el servidor...");
+    //     exit(EXIT_FAILURE);
+    // }
+    sendto(socket, buffer, sizeof(buffer), 0, dest, dlen);
+    SocketReceive(socket, response, RESPONSE_SIZE);
+    #if DEBUG == 1
+        printf("%s\n", response);
+    #endif
+    if(strcmp(response, "Authenticated") == 0) {
+       return 1; 
+    }
+    return -1;
+}
+
+void printMenu() {
+    printf("Selecciona la opción que deseas realizar:\n");
+    printf("1) Insertar\n");
+    printf("2) Query\n");
+    printf("3) Join\n");
+    printf("4) Salir\n");
+    printf("?> ");
+}
+
+//main driver program
+int main(int argc, char *argv[])
+{
+    int hSocket, read_size;
+
+    // Remote address
+    struct sockaddr_in serv_addr= {0};
+    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); //Local Host
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(SERVER_PORT);
+
+    // Create socket
+    if((hSocket = SocketCreate()) == -1) {
+        perror("No se pudo crear el socket...\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Establish connection
+    printf("Conectando con base de datos...\n");
+    if(connect(hSocket,(struct sockaddr *) &serv_addr, sizeof(struct sockaddr_in)) < 0) {
+        perror("Error en la conexión.\n");
+        exit(EXIT_FAILURE);
+    }
+    printf("Conexión exitosa\n");
+    
+    // Input User
+    User usr = inputUser();
+    printf("Verificando credenciales...\n");
+
+    // Authenticate
+    if(authenticate(hSocket, (struct sockaddr *) &serv_addr, sizeof(struct sockaddr_in), usr) < 0) {
+        perror("Credenciales inválidas!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Authenticated -- Run program
+    do {
+        printMenu();
+        // printf("Enter the Message: ");
+        // gets(SendToServer);
+        // //Send data to the server
+        // SocketSend(hSocket, SendToServer, strlen(SendToServer));
+        // //Received the data from the server
+        // read_size = SocketReceive(hSocket, server_reply, 200);
+        // printf("Server Response : %s\n\n",server_reply);
+        fflush(stdin);
+    } while(getc(stdin) != '4');
+
+    close(hSocket);
+    shutdown(hSocket,0);
+    shutdown(hSocket,1);
+    shutdown(hSocket,2);
+    return 0;
+}
---
+++ serverJalador.c
@@ -0,0 +1,418 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+#include<signal.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<syslog.h>
+
+
+#define DEBUG 1
+#define MAX 50
+#define PORT 8083
+
+static void daemonize()
+{
+    pid_t pid;
+    
+    /* Fork off the parent process */
+    pid = fork();
+    
+    /* An error occurred */
+    if (pid < 0)
+        exit(EXIT_FAILURE);
+    
+     /* Success: Let the parent terminate */
+    if (pid > 0)
+        exit(EXIT_SUCCESS);
+    
+    /* On success: The child process becomes session leader */
+    if (setsid() < 0)
+        exit(EXIT_FAILURE);
+    
+    /* Catch, ignore and handle signals */
+    /*TODO: Implement a working signal handler */
+    signal(SIGCHLD, SIG_IGN);
+    signal(SIGHUP, SIG_IGN);
+    
+    /* Fork off for the second time*/
+    pid = fork();
+    
+    /* An error occurred */
+    if (pid < 0)
+        exit(EXIT_FAILURE);
+    
+    /* Success: Let the parent terminate */
+    if (pid > 0)
+        exit(EXIT_SUCCESS);
+    
+    /* Set new file permissions */
+    umask(0);
+    
+    /* Change the working directory to the root directory */
+    /* or another appropriated directory */
+    chdir("/home/nestor/Documents");
+    
+    /* Close all open file descriptors */
+    int x;
+    for (x = sysconf(_SC_OPEN_MAX); x>=0; x--)
+    {
+        close (x);
+    }
+    
+    /* Open the log file */
+    openlog ("SERVER", LOG_PID, LOG_DAEMON);
+}
+
+typedef struct{
+    char username[MAX];
+    char password[MAX];
+} User;
+
+typedef struct {
+    char pid[MAX];
+    char pname[MAX];
+    float price;
+    char description[MAX];
+} Product;
+
+typedef struct {
+    char oid[MAX];
+    char pid[MAX];
+    int qty;
+} Order;
+
+
+
+struct UserNode {
+	User *usr;
+	struct UserNode* next;
+};
+
+struct ProductNode {
+	Product *product;
+	struct ProductNode* next;
+};
+
+struct OrderNode {
+	Order *order;
+	struct OrderNode* next;
+};
+
+struct UserList {
+	int size;
+	struct UserNode* head;
+	struct UserNode* tail;
+};
+
+struct ProductList {
+	int size;
+	struct ProductNode* head;
+	struct ProductNode* tail;
+};
+
+struct OrderList {
+	int size;
+	struct OrderNode* head;
+	struct OrderNode* tail;
+};
+
+struct UserList users;
+struct ProductList products;
+struct OrderList orders;
+
+
+short SocketCreate(void)
+{
+    short hSocket;
+    printf("Create the socket\n");
+    syslog(LOG_NOTICE, "Create the socket");
+    hSocket = socket(AF_INET, SOCK_STREAM, 0);
+    return hSocket;
+}
+
+// Insert user to list
+void insertUser(User* tmpUsr) {
+	struct UserNode* userNode;
+	userNode = (struct UserNode*) malloc(sizeof(struct UserNode));
+    User* newUser = (User *) malloc(sizeof(User));
+    memcpy(newUser, tmpUsr, sizeof(User));
+
+	userNode->usr = newUser;
+	userNode->next = NULL;
+
+	if(users.head == NULL) {
+		users.head = userNode;
+		users.tail = userNode;
+	}
+	else {
+		users.tail->next = userNode;
+		users.tail = users.tail->next;
+	}
+	users.size++;
+}
+void insertOrder(Order *tmpOrder) {
+	struct OrderNode* orderNode;
+	orderNode = (struct OrderNode*) malloc(sizeof(struct OrderNode));
+    Order* newOrder = (Order *) malloc(sizeof(Order));
+    memcpy(newOrder, tmpOrder, sizeof(Order));
+
+	orderNode->order = newOrder;
+	orderNode->next = NULL;
+
+	if(orders.head == NULL) {
+		orders.head = orderNode;
+		orders.tail = orderNode;
+	}
+	else {
+		orders.tail->next = orderNode;
+		orders.tail = orders.tail->next;
+	}
+	orders.size++;
+}
+
+void insertProduct(Product *tmpProduct) {
+	struct ProductNode* productNode;
+	productNode = (struct ProductNode*) malloc(sizeof(struct ProductNode));
+    Product* newProduct = (Product *) malloc(sizeof(Product));
+    memcpy(newProduct, tmpProduct, sizeof(Product));
+
+	productNode->product = newProduct;
+	productNode->next = NULL;
+
+	if(products.head == NULL) {
+		products.head = productNode;
+		products.tail = productNode;
+	}
+	else {
+		products.tail->next = productNode;
+		products.tail = products.tail->next;
+	}
+	products.size++;
+}
+
+void readOrders() {
+    FILE *file;
+    char* fileName = "orders.txt";
+    Order* order = (Order*) malloc(sizeof(Order));
+
+    file = fopen(fileName, "r+");
+	if(file == NULL) {
+        perror("Error in fopen\n");
+        exit(EXIT_FAILURE);
+	}
+    orders.size = 0;
+    #if DEBUG == 1
+    printf("File opened sucessfully\n");
+    #endif
+    while(!feof(file)) {
+        memset(order->oid, '\0', sizeof(order->oid));
+        memset(order->pid, '\0', sizeof(order->pid));
+        fscanf(file, "%s %s %d", order->oid, order->pid, &order->qty);
+        insertOrder(order);
+    }
+    if(fclose(file) != 0) {
+        perror("Error in fclose\n");
+        exit(EXIT_FAILURE);
+    }
+    printf("File closed sucessfully\n");
+}
+
+void readProducts() {
+    FILE *file;
+    char* fileName = "products.txt";
+    Product* product = (Product*) malloc(sizeof(Product));
+
+    file = fopen(fileName, "r+");
+	if(file == NULL) {
+        perror("Error in fopen\n");
+        exit(EXIT_FAILURE);
+	}
+    products.size = 0;
+    #if DEBUG == 1
+    printf("File opened sucessfully\n");
+    #endif
+    while(!feof(file)) {
+        memset(product->pid, '\0', sizeof(product->pid));
+        memset(product->pname, '\0', sizeof(product->pname));
+        memset(product->description, '\0', sizeof(product->description));
+        fscanf(file, "%s %s %f %[^\n]", product->pid, product->pname, &product->price, product->description);
+        insertProduct(product);
+    }
+    if(fclose(file) != 0) {
+        perror("Error in fclose\n");
+        exit(EXIT_FAILURE);
+    }
+    printf("File closed sucessfully\n");
+}
+
+void readConfig() {
+    FILE *file;
+    char* fileName = "config.txt";
+    User* usr = (User*) malloc(sizeof(User));
+
+
+    file = fopen(fileName, "r+");
+	if(file == NULL) {
+        perror("Error in fopen\n");
+        syslog(LOG_NOTICE, "Error in fopen");
+        exit(EXIT_FAILURE);
+	}
+    users.size = 0;
+    #if DEBUG == 1
+    printf("File opened sucessfully\n");
+    syslog(LOG_NOTICE, "Filed opened succesfully");
+    #endif
+    while(!feof(file)) {
+        memset(usr->username, '\0', sizeof(usr->username));
+        memset(usr->password , '\0', sizeof(usr->password));
+        fscanf(file, "%s %s", usr->username, usr->password);
+        insertUser(usr);
+    }
+    if(fclose(file) != 0) {
+        perror("Error in fclose\n");
+        syslog(LOG_NOTICE, "Error in fclose");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void printUsers() {
+	struct UserNode *curr = users.head;
+	while(curr != NULL) {
+        printf("%s %s\n", curr->usr->username, curr->usr->password);
+        curr = curr->next;
+	}
+}
+
+void printProducts() {
+	struct ProductNode *curr = products.head;
+	while(curr != NULL) {
+        printf("%s %s %f %s\n", curr->product->pid, curr->product->pname, curr->product->price, curr->product->description);
+        curr = curr->next;
+	}
+}
+
+void printOrders() {
+	struct OrderNode *curr = orders.head;
+	while(curr != NULL) {
+        printf("%s %s %d\n", curr->order->oid, curr->order->pid, curr->order->qty);
+        curr = curr->next;
+	}
+}
+
+int BindCreatedSocket(int hSocket)
+{
+    int iRetval=-1;
+    struct sockaddr_in  remote= {0};
+    /* Internet address family */
+    remote.sin_family = AF_INET;
+    /* Any incoming interface */
+    remote.sin_addr.s_addr = htonl(INADDR_ANY);
+    remote.sin_port = htons(PORT); /* Local port */
+    iRetval = bind(hSocket,(struct sockaddr *)&remote,sizeof(remote));
+    return iRetval;
+}
+
+User* castToUser(unsigned char* buffer, User usr) {
+    return (User *) buffer;
+}
+//validar usuarios
+int validate(User usr){
+	struct UserNode *curr = users.head;
+	while(curr != NULL){
+		if(strcmp(curr->usr->username, usr.username) == 0 && strcmp(curr->usr->password, usr.password) == 0){
+			return 1;
+		}
+		curr = curr->next;
+	}
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    int socket_desc, sock, clientLen, read_size;
+
+    struct sockaddr_in client;
+    char client_message[2000]= {0};
+    char message[100] = {0};
+    const char *pMessage = "marcelo";
+    //Create socket
+    socket_desc = SocketCreate();
+    if (socket_desc == -1) {
+        perror("Could not create socket\n");
+        syslog(LOG_NOTICE, "Could NOt create socket");
+        exit(EXIT_FAILURE);
+    }
+    printf("Socket created\n");
+    syslog(LOG_NOTICE, "Socket created");
+
+    //Bind
+    if( BindCreatedSocket(socket_desc) < 0) {
+        //print the error message
+        perror("Bind failed.\n");
+        syslog(LOG_NOTICE, "Bind failed");
+        return 1;
+    }
+    printf("Bind done\n");
+    syslog(LOG_NOTICE, "Bind Done");
+    //Listen
+    listen(socket_desc, 3);
+
+    // Read config file
+    readConfig();
+    readProducts();
+    readOrders();
+
+    //Accept an incoming connection
+    while(1)
+    {
+        printf("Waiting for incoming connections...\n");
+        clientLen = sizeof(struct sockaddr_in);
+
+
+        //accept connection from an incoming client
+        sock = accept(socket_desc,(struct sockaddr *)&client,(socklen_t*)&clientLen);
+        if (sock < 0) {
+            perror("Accept failed");
+            return 1;
+        }
+        printf("Connection accepted\n");
+        syslog(LOG_NOTICE, "Connection Accepted");
+
+        //Receive a reply from the client
+        while(recv(sock, client_message, sizeof(User), 0) > -1) {
+            User usr;
+            memcpy(&usr,client_message,sizeof(User));
+
+            #if DEBUG == 1
+            printf("%s\n", usr.username);
+            #endif
+
+            // Routine
+            //pMessage hard coded. funcion nueva
+            
+            
+            if(validate(usr) == 1)
+            {
+                strcpy(message,"Authenticated");
+                send(sock, message, strlen(message), 0);
+            } else {
+    		 strcpy(message,"Login failed");
+                send(sock, message, strlen(message), 0);
+                
+                break;
+            }
+            
+            
+            memset(client_message, '\0', sizeof client_message);
+            memset(message, '\0', sizeof message);
+            sleep(1);
+        }
+        printf("Closing socket...\n");
+        close(sock);
+    }
+    return 0;
+}
